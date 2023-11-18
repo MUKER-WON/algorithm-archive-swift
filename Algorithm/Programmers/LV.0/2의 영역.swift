943503cde6ba077245f27904265a2044077b5e4d
MUKER-WON <muker.dev@gmail.com>
2023-11-18 17:19:48 +0900

    style: 파일 스타일 수정

--- Algorithm/Programmers/LV.0/2의 영역.swift
+++ Algorithm/Programmers/LV.0/2의 영역.swift
@@ -8,7 +8,14 @@
 import Foundation
 
 func LV0_2의_영역() {
-	
+	/// 문제 풀이
+	func solution(_ arr:[Int]) -> [Int] {
+		guard let firstIndex = arr.firstIndex(of: 2),
+					let lastIndex = arr.lastIndex(of: 2) else {
+			return [-1]
+		}
+		return Array(arr[firstIndex...lastIndex])
+	}
 	/// 테스트 케이스
 	let cases: [[Int]] = [
 		[1, 2, 1, 4, 5, 2, 9],
@@ -16,17 +23,6 @@ func LV0_2의_영역() {
 		[1, 1, 1],
 		[1, 2, 1, 2, 1, 10, 2, 1],
 	]
-	
-	/// 문제 풀이
-	func solution(_ arr:[Int]) -> [Int] {
-		guard let firstIndex = arr.firstIndex(of: 2),
-					let lastIndex = arr.lastIndex(of: 2) else {
-			return [-1]
-		}
-		
-		return Array(arr[firstIndex...lastIndex])
-	}
-	
 	cases.forEach {
 		print("\(solution($0))")
 	}
