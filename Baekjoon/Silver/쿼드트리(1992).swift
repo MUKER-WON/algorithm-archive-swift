3fdc205ae1ec6eec9f6e6216e1ec9a39a2378c70
MUKER-WON <muker.dev@gmail.com>
2023-11-28 15:31:06 +0900

    add: 문제 복습 및 코드 추가

--- Baekjoon/Silver/쿼드트리(1992).swift
+++ Baekjoon/Silver/쿼드트리(1992).swift
@@ -48,3 +48,45 @@ func silver_쿼드트리() {
 	divideConquer(x: 0, y: 0, size: N)
 	print(result.joined())
 }
+
+/// 1try와 달라진 점은 재귀 들어가는 각각의 함수 4개를 for문으로 바꿨다.
+func silver_쿼드트리_try2() {
+	let N = Int(readLine()!)!
+	var arr = [[Character]]()
+	var result = ""
+
+	for _ in 0..<N {
+		arr.append(Array(readLine()!))
+	}
+
+	func QuadTree(y: Int, x: Int, size: Int) {
+		let start = arr[y][x]
+		var equal = true
+		
+	loop: for y in y..<y+size {
+		for x in x..<x+size {
+			if arr[y][x] != start {
+				equal = false
+				break loop
+			}
+		}
+	}
+		
+		if equal {
+			result.append(start)
+		} else {
+			let new = size / 2
+			result.append("(")
+			for i in stride(from: y, to: y+size, by: new) {
+				for j in stride(from: x, to: x+size, by: new) {
+					QuadTree(y: i, x: j, size: new)
+				}
+			}
+			result.append(")")
+		}
+		
+	}
+
+	QuadTree(y: 0, x: 0, size: N)
+	print(result)
+}
