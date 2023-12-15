4f5f7ada44fcd61d2f6091e0a99d927b44386899
MUKER-WON <muker.dev@gmail.com>
2023-12-15 16:05:10 +0900

    add: 백준_지름길(1446)

--- main.swift
+++ main.swift
@@ -1,1 +1,22 @@
+import Foundation
 
+
+
+
+//func get() {
+//	let runloop = CFRunLoopG	etCurrent()
+//	print("시작")
+//	guard let url = URL(string: "https://itunes.apple.com/search?term=marvel&media=movie") else {
+//		return
+//	}
+//	var request = URLRequest(url: url)
+//	request.httpMethod = "GET"
+//	URLSession.shared
+//	URLSession.shared.dataTask(with: request) { d, response, error in
+//		guard let d else { return }
+//		print(d)
+//	}.resume()
+//}
+//
+//get()
+//dispatchMain()
