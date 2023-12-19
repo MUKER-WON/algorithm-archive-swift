713d52ec19a53087da46c825655ea55f690e8193
MUKER-WON <muker.dev@gmail.com>
2023-12-19 22:28:14 +0900

    add: 백준-녹색 옷 입은 애가 젤다지?

---
+++ Baekjoon/Gold/녹색 옷 입은 애가 젤다지?(4485).swift
@@ -0,0 +1,77 @@
+//
+//  녹색 옷 입은 애가 젤다지?(4485).swift
+//  Algorithm
+//
+//  Created by Muker on 12/19/23.
+//
+
+import Foundation
+
+//MARK: - 성공 풀이 1
+
+func 백준_녹색옷입은애가젤다지_1() {
+	let D = [(0,1),(0,-1),(1,0),(-1,0)]
+	var str = ""
+
+	for i in 1... {
+		let N = Int(readLine()!)!
+		guard N != 0 else { break }
+		
+		let A = (0..<N).map { _ in readLine()!.split { $0 == " " }.map { Int($0)! } }
+		var V = Array(repeating: Array(repeating: false, count: N), count: N)
+		var C = Array(repeating: Array(repeating: Int.max, count: N), count: N)
+		var Q = [(0,0)]
+		C[0][0] = A[0][0]
+		
+		while !Q.isEmpty {
+			let node = Q.popLast()!
+			let (y,x) = (node.0,node.1)
+			// 방문처리
+			guard !V[y][x] else { continue }
+			V[y][x] = true
+			// 상하좌우 그래프 탐색
+			for j in 0..<4 {
+				let (ny,nx) = (D[j].0+y, D[j].1+x)
+				guard ny >= 0 && ny < N && nx >= 0 && nx < N && !V[ny][nx] else { continue }
+				C[ny][nx] = min(C[ny][nx],C[y][x]+A[ny][nx])
+				Q.append((ny,nx))
+			}
+			Q.sort { C[$0.0][$0.1] > C[$1.0][$1.1] }
+		}
+		str += "Problem \(i): \(C[N-1][N-1])\n"
+	}
+	print(str)
+}
+
+
+//MARK: - 실패 풀이
+
+func 백준_녹색옷입은애가젤다지_시간초과1() {
+	let D = [(0,1),(0,-1),(1,0),(-1,0)]
+
+	for i in 1... {
+		let N = Int(readLine()!)!
+		guard N != 0 else { break }
+		let A = (0..<N).map { _ in readLine()!.split { $0 == " " }.map { Int($0)! } }
+		var result = Int.max
+		
+		func task(y: Int, x: Int, arr: [[Int]], pay: Int) {
+			var newArr = arr
+			let newpay = pay + arr[y][x]
+			newArr[y][x] = -1
+			
+			if (y,x) == (N-1,N-1) {
+				result = min(newpay, result)
+				return
+			}
+			
+			for i in 0..<4 {
+				let (ny,nx) = (y+D[i].0,x+D[i].1)
+				guard ny >= 0 && ny < N && nx >= 0 && nx < N && arr[ny][nx] >= 0 else { continue }
+				task(y: ny, x: nx, arr: newArr, pay: newpay)
+			}
+		}
+		task(y: 0, x: 0, arr: A, pay: 0)
+		print("Problem \(i): \(result)")
+	}
+}
