beecf3a9b2030f9b09f60292eba2b8772ef90a36
MUKER-WON <muker.dev@gmail.com>
2024-01-01 02:10:01 +0900

    [Add] 복습 코드

--- Baekjoon/Gold/게리맨더링2(17779).swift
+++ Baekjoon/Gold/게리맨더링2(17779).swift
@@ -48,3 +48,46 @@ func 백준_게리맨더링2_1() {
 
 	print(ans)
 }
+
+//MARK: - 풀이 2
+
+func 백준_게리맨더링2_2() {
+	let n = Int(readLine()!)!
+	let map = (0..<n).map { _ in readLine()!.split { $0 == " " }.map { Int($0)! } }
+	var ans = Int.max
+	var myMap = Array(repeating: Array(repeating: 0, count: n), count: n)
+
+	func task(_ x: Int, _ y: Int, _ d1: Int, _ d2: Int) {
+		var citizen = [0,0,0,0,0]
+		
+		for r in 0..<n {
+			for c in 0..<n {
+				if x+y <= r+c, r+c <= x+y+2*d2 && y-x-2*d1 <= c-r, c-r <= y-x {
+					citizen[4] += map[r][c]
+				} else if r >= 0 && x+d1 > r && c >= 0 && y >= c {
+					citizen[0] += map[r][c]
+				} else if r >= 0 && x+d2 >= r && c > y && n >= c {
+					citizen[1] += map[r][c]
+				} else if r >= x+d1 && n >= r && c >= 0 && y-d1+d2 > c {
+					citizen[2] += map[r][c]
+				} else if r > x+d2 && n >= r && c >= y-d1+d2 && n >= c {
+					citizen[3] += map[r][c]
+				}
+			}
+		}
+		print(citizen.max()! - citizen.min()!)
+		ans = min(ans ,citizen.max()! - citizen.min()!)
+	}
+
+	for x in 1..<n {
+		for y in 1..<n {
+			for d1 in 1..<n {
+				for d2 in 1..<n {
+					task(x-1, y-1, d1, d2)
+				}
+			}
+		}
+	}
+
+	print(ans)
+}
