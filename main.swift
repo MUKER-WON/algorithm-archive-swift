507f0b9336a6bbcadd21b10255f0cfa609e2f543
MUKER-WON <muker.dev@gmail.com>
2024-01-29 13:13:42 +0900

    Update main.swift

--- main.swift
+++ main.swift
@@ -4,48 +4,4 @@
 //
 //
 
-let n = Int(readLine()!)!
-let chessBoard = (0..<n).map { _ in readLine()!.split { $0 == " " }.map { Int($0)! } }
 
-let diagonalNum = 2*n-1
-var list = Array(repeating: [(Int,Int)](), count: diagonalNum)
-var visited = Array(repeating: false, count: diagonalNum)
-var ans = 0
-
-for i in 0..<n {
-	for j in 0..<n {
-		if chessBoard[i][j] == 1 {
-			list[i+j].append((i,j))
-		}
-	}
-}
-
-func dfs(n: Int, cnt: Int) {
-	// 남은 자리 다 놓아도 정답이 갱신 안되는 경우(가지치기)
-	if ans >= cnt+(diagonalNum+1-n)/2 {
-		return
-	}
-	
-	if n >= diagonalNum {
-		ans = max(ans, cnt)
-		return
-	}
-	
-	for (i,j) in list[n] {
-		let index = i-j >= 0 ? i-j : diagonalNum+(i-j)
-		if visited[index] == false {
-			visited[index] = true
-			dfs(n: n+2, cnt: cnt+1)
-			visited[index] = false
-		}
-	}
-	
-	dfs(n: n+2, cnt: cnt)
-}
-
-dfs(n: 0, cnt: 0)
-let ans1 = ans
-ans = 0
-dfs(n: 1, cnt: 0)
-
-print(ans1 + ans)
