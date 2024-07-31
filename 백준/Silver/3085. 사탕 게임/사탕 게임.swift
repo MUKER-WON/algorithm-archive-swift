e877518bcb85ea5598bc60492917532a4bdb82e3
Kang Muk <muker.dev@gmail.com>
2024-07-31 22:03:44 +0900

    [Silver II] Title: 사탕 게임, Time: 20 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.swift
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.swift
@@ -5,33 +5,28 @@ let G = (0..<N).map { _ in
 }
 var ans = 0
 
-func check(_ arr: [String]) -> Int {
+func C(_ arr: [String]) -> Int {
     var (maxCnt, now) = (0,1)
     for i in 1..<N {
-        if arr[i] == arr[i-1] {
-            now += 1
-        } else {
-            maxCnt = max(maxCnt, now)
-            now = 1
-        }
+        if arr[i] == arr[i-1] { now += 1 }
+        else { maxCnt = max(maxCnt, now); now = 1 }
     }
-    maxCnt = max(maxCnt, now)
-    return maxCnt
+    return max(maxCnt, now)
 }
 
 for y in 0..<N {
-    ans = max(ans, [check(G[y]), check((0..<N).map { G[y][$0] })].max()!)
+    ans = max(ans, [C(G[y]), C((0..<N).map { G[y][$0] })].max()!)
     for x in 0..<N {
         let (cY,cX) = (y+1,x+1)
         if cY < N && G[y][x] != G[cY][x] {
             var G = G
             (G[y][x],G[cY][x]) = (G[cY][x],G[y][x])
-            ans = max(ans, [check(G[y]), check(G[cY]), check((0..<N).map { G[$0][x] })].max()!)
+            ans = max(ans, [C(G[y]), C(G[cY]), C((0..<N).map { G[$0][x] })].max()!)
         }
         if cX < N && G[y][x] != G[y][cX] {
             var G = G
             (G[y][x],G[y][cX]) = (G[y][cX],G[y][x])
-            ans = max(ans, [check(G[y]), check((0..<N).map { G[$0][cX] }), check((0..<N).map { G[$0][x] })].max()!)
+            ans = max(ans, [C(G[y]), C((0..<N).map { G[$0][cX] }), C((0..<N).map { G[$0][x] })].max()!)
         }
     }
 }
