b1615adec33eed9768609f2a4466fc34987ec84c
Kang Muk <muker.dev@gmail.com>
2024-08-06 08:49:24 +0900

    [Bronze V] Title: 나머지, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+var input = readLine()!.split(separator: " ").map { Int($0)! }
+
+print((input[0] + input[1]) % input[2])
+print(((input[0] % input[2]) + (input[1] % input[2])) % input[2])
+print((input[0] * input[1]) % input[2])
+print(((input[0] % input[2]) * (input[1] % input[2])) % input[2])
