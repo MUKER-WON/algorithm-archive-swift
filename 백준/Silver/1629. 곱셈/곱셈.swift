57389374944bbd8b600c802edb30fb571b527392
Kang Muk <muker.dev@gmail.com>
2024-09-11 19:28:51 +0900

    [Silver I] Title: 곱셈, Time: 4 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.swift
+++ 백준/Silver/1629. 곱셈/곱셈.swift
@@ -1,9 +1,9 @@
 let I = readLine()!.split { $0 == " " }.map { Int($0)! }
+let (A,B,C) = (I[0],I[1],I[2])
 
-func task(_ a: Int, _ b: Int, _ m: Int) -> Int {
+func task(_ a: Int, _ b: Int) -> Int {
     if b == 0 { return 1 }
-    let n = task(a, b/2, m)
-    return b % 2 == 0 ? (n*n) % m : (n*n % m) * a % m
+    let n = task(a, b/2)
+    return b % 2 == 0 ? (n*n) % C : (n*n % C) * a % C
 }
-
-print(task(I[0],I[1],I[2]))
+print(task(A,B))
