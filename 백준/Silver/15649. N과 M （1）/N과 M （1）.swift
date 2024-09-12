3544c11a5687eccace7d05005babfb01b6778f32
Kang Muk <muker.dev@gmail.com>
2024-09-12 13:57:39 +0900

    [Silver III] Title: N과 M (1), Time: 36 ms, Memory: 71080 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.swift
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.swift
@@ -1,20 +1,21 @@
-let NM = readLine()!.split(separator: " ").map { Int($0)! }
-let N = NM[0], M = NM[1]
-var used = Array(repeating: false, count: N+1)
-var result = ""
+let I = readLine()!.split { $0 == " " }.map { Int($0)! }
+let (N,M) = (I[0],I[1])
+var visited = Array(repeating: false, count: N+1)
+var ans = ""
 
-func backtrack(s: String, count: Int) {
+func task(_ s: String, _ count: Int) {
     if count == M {
-        result.append(s+"\n")
+        ans += s+"\n"
         return
     }
     for i in 1...N {
-        if !used[i] {
-            used[i] = true
-            backtrack(s: s+"\(i) ", count: count+1)
-            used[i] = false
+        if !visited[i] {
+            visited[i] = true
+            task(s+"\(i) ", count+1)
+            visited[i] = false
         }
     }
 }
-backtrack(s: "", count: 0)
-print(result)
+
+task("", 0)
+print(ans)
