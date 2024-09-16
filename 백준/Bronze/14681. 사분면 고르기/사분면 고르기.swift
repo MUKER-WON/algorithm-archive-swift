9a2c8c59e253f3ac78cca321cedb40465437690c
Kang Muk <muker.dev@gmail.com>
2024-09-16 22:43:28 +0900

    [Bronze V] Title: 사분면 고르기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.swift
@@ -0,0 +1,16 @@
+var input = Int(readLine()!)!
+var input2 = Int(readLine()!)!
+
+if input > 0 {
+    if input2 > 0 {
+        print("1")
+    } else {
+        print("4")
+    }
+} else {
+    if input2 > 0 {
+        print("2")
+    } else {
+        print("3")
+    }
+}
