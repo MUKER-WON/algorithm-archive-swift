3dd424a90a94b8cb575462e1d6a970e65b6e63a1
Kang Muk <muker.dev@gmail.com>
2024-09-20 22:52:29 +0900

    [Silver III] Title: 빈도 정렬, Time: 8 ms, Memory: 69112 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.swift
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.swift
@@ -1,20 +1,14 @@
-let I = readLine()!.split { $0 == " " }.map { Int($0)! }
-let (N,C) = (I[0],I[1])
+_ = readLine()
 var dic = [Int: (Int,Int)]()
-var cnt = 1
 var ans = ""
 
-readLine()!.split { $0 == " " }.forEach {
-    let n = Int(String($0))!
-    if dic[n] == nil { dic[n] = (cnt,0) }
-    dic[n] = (dic[n]!.0, dic[n]!.1+1)
-    cnt += 1
+readLine()!.split { $0 == " " }.enumerated().forEach {
+    let n = Int(String($0.element))!
+    dic[n, default: ($0.offset, 0)].1 += 1
 }
 
-dic.sorted {
+print(dic.sorted {
     $0.value.1 != $1.value.1 ? $0.value.1 > $1.value.1 : $0.value.0 < $1.value.0
-}.forEach {
-    ans += String(repeating: "\($0.key) ", count: $0.value.1)
-}
-
-print(ans)
+}.map {
+    String(repeating: "\($0.key) ", count: $0.value.1)
+}.joined())
