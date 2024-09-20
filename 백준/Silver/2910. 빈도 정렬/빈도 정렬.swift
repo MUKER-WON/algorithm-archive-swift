d73deea2ee8bf998acb9c28a4674bb6fcdabd8d6
Kang Muk <muker.dev@gmail.com>
2024-09-20 22:11:21 +0900

    [Silver III] Title: 빈도 정렬, Time: 8 ms, Memory: 69116 KB -BaekjoonHub

---
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.swift
@@ -0,0 +1,18 @@
+let I = readLine()!.split { $0 == " " }.map { Int($0)! }
+let (N,C) = (I[0],I[1])
+var dic = [Int: (Int,Int)]()
+var cnt = 1
+var ans = ""
+
+readLine()!.split { $0 == " " }.forEach {
+    let n = Int(String($0))!
+    if dic[n] == nil { dic[n] = (cnt,0) }
+    dic[n] = (dic[n]!.0, dic[n]!.1+1)
+    cnt += 1
+}
+
+var a = dic.sorted { $0.value.1 != $1.value.1 ? $0.value.1 > $1.value.1 : $0.value.0 < $1.value.0 }
+
+a.forEach { ans += Array(repeating: String($0.key), count: $0.value.1).joined(separator: " ") + " " }
+
+print(ans)
