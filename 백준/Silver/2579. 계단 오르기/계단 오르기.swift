1a36b24501ee9c738c12c56f044270feb5f51477
Kang Muk <muker.dev@gmail.com>
2024-09-23 22:39:14 +0900

    [Silver III] Title: 계단 오르기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.swift
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.swift
@@ -1,11 +1,11 @@
 let n = Int(readLine()!)!
-var arr = [0]+(0..<n).map { _ in Int(readLine()!)! }
+var step = [0]+(0..<n).map { _ in Int(readLine()!)! }
 var dp = Array(repeating: 0, count: n+1)
 
 for i in 1...n {
-    if i == 1 { dp[i] = arr[i]; continue }
-    if i == 2 { dp[i] = max(dp[i-1]+arr[i], dp[i-2]+arr[i]); continue }
-    dp[i] = max(dp[i-3]+arr[i-1]+arr[i], dp[i-2]+arr[i])
+    if i == 1 { dp[i] = step[i]; continue }
+    if i == 2 { dp[i] = dp[i-1]+step[i]; continue }
+    dp[i] = max(dp[i-3]+step[i-1]+step[i], dp[i-2]+step[i])
 }
 
 print(dp[n])
