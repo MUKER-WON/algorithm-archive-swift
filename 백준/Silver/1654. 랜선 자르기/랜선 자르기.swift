393b9760e7f784024c0d5fd236a4a5892934d0c6
Kang Muk <muker.dev@gmail.com>
2024-09-27 00:41:00 +0900

    [Silver II] Title: 랜선 자르기, Time: 12 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.swift
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.swift
@@ -1,14 +1,11 @@
 let I = readLine()!.split { $0 == " " }.map { Int($0)! }
-let (K, N) = (I[0], I[1])
-let arr = (0..<K).map { _ in Int(readLine()!)! }
+let arr = (0..<I[0]).map { _ in Int(readLine()!)! }
 var (st, en) = (1, arr.max()!)
-
 while st <= en {
     var cnt = 0
     let mid = (st + en) / 2
     for i in arr { cnt += (i / mid) }
-    if cnt < N { en = mid - 1 }
+    if cnt < I[1] { en = mid - 1 }
     else { st = mid + 1 }
 }
-
 print(en)
