5ad66486ff023e0e2582af8c1345950b3ac7d729
Kang Muk <muker.dev@gmail.com>
2024-09-27 20:48:51 +0900

    [Gold V] Title: 수 고르기, Time: 56 ms, Memory: 71068 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.swift
+++ 백준/Gold/2230. 수 고르기/수 고르기.swift
@@ -4,9 +4,13 @@ let A = (0..<N).map { _ in Int(readLine()!)! }.sorted()
 var (p1, p2) = (0,0)
 var ans = Int.max
 
-while p1 < N && p2 < N {
+while p2 < N {
     let n = A[p2] - A[p1]
-    if n >= M {
+    if n == M {
+		    ans = M
+		    break
+    }
+    if n > M {
         ans = min(n, ans)
         p1 += 1
     } else {
