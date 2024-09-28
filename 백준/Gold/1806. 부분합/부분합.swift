001a036b15ff7489d680df33e19c4009e116eac7
Kang Muk <muker.dev@gmail.com>
2024-09-28 14:22:34 +0900

    [Gold IV] Title: 부분합, Time: 36 ms, Memory: 76292 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.swift
+++ 백준/Gold/1806. 부분합/부분합.swift
@@ -1,19 +1,20 @@
 let I = readLine()!.split { $0 == " " }.map { Int($0)! }
 let (N, S) = (I[0], I[1])
-var A = readLine()!.split { $0 == " " }.map { Int($0)! }
-var p2 = 0
+let A = readLine()!.split { $0 == " " }.map { Int($0)! }
+var (p1, p2) = (0,0)
 var sum = 0
 var ans = Int.max
 
-for p1 in 0..<N {
-    while p2 < N && sum < S {
+while true {
+    if sum >= S {
+        ans = min(ans, p2-p1)
+        sum -= A[p1]
+        p1 += 1
+    } else {
+        if p2 == N { break }
         sum += A[p2]
         p2 += 1
     }
-    if sum >= S {
-        ans = min(ans, p2 - p1)
-    }
-    sum -= A[p1]
 }
 
 print(ans == Int.max ? 0 : ans)
