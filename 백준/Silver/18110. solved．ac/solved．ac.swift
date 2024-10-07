756fb5698c2583eed9da7cdf2c1b7a1a3a4f199a
Kang Muk <muker.dev@gmail.com>
2024-10-08 01:31:14 +0900

    [Silver IV] Title: solved.ac, Time: 124 ms, Memory: 85372 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.swift
+++ 백준/Silver/18110. solved．ac/solved．ac.swift
@@ -2,5 +2,4 @@ import Foundation
 let n = Int(readLine()!)!
 let div = Int(round(Double(n) * 0.15))
 let arr = (0..<n).map { _ in Int(readLine()!)! }.sorted()
-let avg = (div..<n-div).map { arr[$0] }.reduce(0,+)
-print(n == 0 ? 0 : Int(round(Double(avg) / Double(n-div*2))))
+print(n == 0 ? 0 : Int(round(Double(arr[div..<n-div].reduce(0,+)) / Double(n-div*2))))
