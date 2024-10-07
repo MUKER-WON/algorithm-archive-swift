9abebca5ba005e901b07bf6179aa8ea0ce0e7955
Kang Muk <muker.dev@gmail.com>
2024-10-08 01:25:29 +0900

    [Silver IV] Title: solved.ac, Time: 124 ms, Memory: 85372 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.swift
+++ 백준/Silver/18110. solved．ac/solved．ac.swift
@@ -1,7 +1,6 @@
 import Foundation
-
 let n = Int(readLine()!)!
-let average = Int(round(Double(n) * 0.15))
+let avg = Int(round(Double(n) * 0.15))
 let arr = (0..<n).map { _ in Int(readLine()!)! }.sorted()
-let level = (average..<n-average).map { arr[$0] }.reduce(0,+)
-print(n == 0 ? 0 : Int(round(Double(level) / Double(n-average*2))))
+let level = (avg..<n-avg).map { arr[$0] }.reduce(0,+)
+print(n == 0 ? 0 : Int(round(Double(level) / Double(n-avg*2))))
