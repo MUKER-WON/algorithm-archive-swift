28cf53ecae40d196a00ea45a324c251ecdc1404e
Kang Muk <muker.dev@gmail.com>
2024-10-12 01:37:37 +0900

    [Silver IV] Title: 상근이의 여행, Time: 56 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.swift
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.swift
@@ -1,5 +1,5 @@
 for _ in 0..<Int(readLine()!)! {
-    let I = readLine()!.split { $0 == " " }.map { Int($0)! }
-    for _ in 0..<I[1] { _ = readLine() }
+    let I = readLine()!.split {$0==" "}.map{Int($0)!}
+    for _ in 0..<I[1] {_ = readLine()}
     print(I[0]-1)
 }
