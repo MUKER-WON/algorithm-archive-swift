007823932d682e5631bb6cbb88ba9f5a4beeff65
Kang Muk <muker.dev@gmail.com>
2024-10-13 10:40:57 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.swift
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.swift
@@ -2,32 +2,22 @@ let I = readLine()!.split { $0 == " " }.map { Int($0)! }
 let (N,M) = (I[0],I[1])
 var dic = [Int: [Int]]()
 var ans = (0, Int.max)
-
 for _ in 1...M {
     let I = readLine()!.split { $0 == " " }.map { Int($0)! }
     dic[I[0], default: []].append(I[1])
     dic[I[1], default: []].append(I[0])
 }
-
 for i in 1...N {
     var q = dic[i]!.map { ($0,1) }
-    var visited = Array(repeating: false, count: N+1)
-    var score = 0
-    
+    var vis = Array(repeating: false, count: N+1)
+    var scr = 0
     while !q.isEmpty {
         let (n,v) = q.removeFirst()
-        if visited[n] { continue }
-        visited[n] = true
-        score += v
-        
-        for j in dic[n]! {
-            if visited[j] { continue }
-            q.append((j,v+1))
-        }
+        if vis[n] { continue }
+        vis[n] = true
+        scr += v
+        for j in dic[n]! where !vis[j] { q.append((j,v+1)) }
     }
-    ans = ans.1 >= score 
-    ? (ans.1 == score ? (min(ans.0, i), score)
-    : (i, score)) : ans
+    ans = ans.1 >= scr ? (ans.1 == scr ? (min(ans.0, i), scr) : (i, scr)) : ans
 }
-
 print(ans.0)
