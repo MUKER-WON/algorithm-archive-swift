b3423df8ca4067714902b1667795e324de888366
Kang Muk <muker.dev@gmail.com>
2024-10-13 15:43:03 +0900

    [Silver I] Title: IOIOI, Time: 68 ms, Memory: 85716 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.swift
+++ 백준/Silver/5525. IOIOI/IOIOI.swift
@@ -1,5 +1,14 @@
 let N = Int(readLine()!)!
 let M = Int(readLine()!)!
 let S = readLine()!.map { String($0) }
-let str = "I"+String(repeating: "OI", count: N)
-print((0..<M-N*2).filter { str == S[$0...$0+N*2].joined() }.count)
+var (cnt,ans,i) = (0,0,0)
+
+while i<M-2 {
+    if S[i] == "I" && S[i+1] == "O" && S[i+2] == "I" {
+        cnt += 1
+        if cnt == N { ans += 1; cnt -= 1 }
+        i += 1
+    } else { cnt = 0 }
+    i += 1
+}
+print(ans)
