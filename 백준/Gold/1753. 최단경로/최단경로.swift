fab9af44cb03e05a4c324457b80b2352235e3b1a
Kang Muk <muker.dev@gmail.com>
2024-10-16 01:20:35 +0900

    [Gold IV] Title: 최단경로, Time: 576 ms, Memory: 82608 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.swift
+++ 백준/Gold/1753. 최단경로/최단경로.swift
@@ -1,89 +1,100 @@
-class Heap<T> {
-    var arr = [T]()
-    let sort: (T,T) -> Bool
-    
-    init(sort: @escaping (T,T) -> Bool) { self.sort = sort }
-    
-    func insert(_ v: T) {
-        arr.append(v)
-        siftUp(arr.count-1)
+struct Node: Comparable {
+    static func < (lhs: Node, rhs: Node) -> Bool {
+        lhs.cost < rhs.cost
     }
     
-    func remove() -> T? {
-        guard !arr.isEmpty else { return nil }
-        arr.swapAt(0, arr.count-1)
-        let v = arr.removeLast()
-        siftDown(0)
-        return v
-    }
-    
-    private func siftUp(_ child: Int) {
-        var child = child
-        var parent = parentIndex(child)
-        
-        while child > 0 && sort(arr[child], arr[parent]) {
-            arr.swapAt(child, parent)
-            child = parent
-            parent = parentIndex(child)
-        }
-    }
-    
-    private func siftDown(_ parent: Int) {
-        var parent = parent
-        
-        while true {
-            let leftChild = leftChildIndex(parent)
-            let rightChild = rightChildIndex(parent)
-            var candidate = leftChild
-            
-            guard leftChild < arr.count else { break }
-            
-            if rightChild < arr.count && sort(arr[rightChild], arr[candidate]) {
-                candidate = rightChild
-            }
-            
-            if sort(arr[candidate], arr[parent]) {
-                arr.swapAt(candidate, parent)
-                parent = candidate
-            } else {
-                break
-            }
-        }
-    }
-    
-    func parentIndex(_ i: Int) -> Int { (i-1)/2 }
-    func leftChildIndex(_ i: Int) -> Int { i*2+1 }
-    func rightChildIndex(_ i: Int) -> Int { i*2+2 }
+    let node: Int
+    let cost: Int
 }
 
-let I = readLine()!.split { $0 == " " }.map { Int($0)! }
-let (V,E,K) = (I[0],I[1],Int(readLine()!)!)
-let INF = Int(1e9)
-var graph = [Int: [(Int,Int)]]()
-var shortest = Array(repeating: INF, count: V+1); shortest[K] = 0
+class PriorityQueue {
+    var heap = [Node]()
+    
+    var isEmpty: Bool { heap.isEmpty }
+    
+    func insert(_ value: Node) {
+        heap.append(value)
+        var idx = heap.count - 1
+        
+        while idx > 0 {
+            let parent = (idx - 1) / 2
+            if heap[parent] <= heap[idx] { break }
+            heap.swapAt(parent, idx)
+            idx = parent
+        }
+    }
+    
+    func pop() -> Node? {
+        guard !heap.isEmpty else { return nil }  // 안전하게 처리
+        
+        let result = heap[0]
+        if heap.count == 1 {
+            heap.removeLast()  // 남은 원소가 1개일 때 처리
+            return result
+        }
+        
+        heap[0] = heap.removeLast()
+        var idx = 0
+        
+        while idx < heap.count {
+            let left = 2 * idx + 1
+            let right = 2 * idx + 2
+            var minIdx = idx
+            
+            if left < heap.count && heap[left] < heap[minIdx] {
+                minIdx = left
+            }
+            if right < heap.count && heap[right] < heap[minIdx] {
+                minIdx = right
+            }
+            if minIdx == idx { break }
+            
+            heap.swapAt(idx, minIdx)
+            idx = minIdx
+        }
+        return result
+    }
+}
+
+let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+let (V, E) = (input[0], input[1])
+let K = Int(readLine()!)!
+
+var graph = [[Node]](repeating: [], count: V + 1)
+var distants = [Int](repeating: Int.max, count: V + 1)
 
 for _ in 0..<E {
-    let I = readLine()!.split { $0 == " " }.map { Int($0)! }
-    let (u,v,w) = (I[0],I[1],I[2])
-    graph[u, default: []].append((v,w))
+    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+    let (start, end, cost) = (input[0], input[1], input[2])
+    graph[start].append(Node(node: end, cost: cost))
 }
 
-let heap = Heap<(Int,Int)> { $0.1 < $1.1 }
-heap.insert((K,0))
-
-while let (now, value) = heap.remove() {
-    if value > shortest[now] { continue }
-    if let arr = graph[now] {
-        for (next, nextValue) in arr {
-            let newValue = value + nextValue
-            if newValue < shortest[next] {
-                shortest[next] = newValue
-                heap.insert((next, newValue))
+func dijkstra(node: Int) {
+    let queue = PriorityQueue()
+    
+    distants[node] = 0
+    queue.insert(Node(node: node, cost: 0))
+    
+    while !queue.isEmpty {
+        guard let now = queue.pop() else { break }
+        
+        if distants[now.node] < now.cost { continue }
+        
+        for next in graph[now.node] {
+            let cost = next.cost + now.cost
+            
+            if cost < distants[next.node] {
+                distants[next.node] = cost
+                queue.insert(Node(node: next.node, cost: cost))
             }
         }
     }
 }
 
+dijkstra(node: K)
+
 for i in 1...V {
-    print(shortest[i] == INF ? "INF": "\(shortest[i])")
+    distants[i] == Int.max
+    ? print("INF")
+    : print(distants[i])
 }
