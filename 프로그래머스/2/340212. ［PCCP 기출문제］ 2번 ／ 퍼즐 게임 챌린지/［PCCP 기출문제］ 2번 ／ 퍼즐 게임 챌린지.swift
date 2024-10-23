f259ff469411fd6206174cf7f4220544cdb2a7e9
Kang Muk <muker.dev@gmail.com>
2024-10-24 01:57:49 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 157.67 ms, Memory: 42 MB -BaekjoonHub

---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+func solution(
+    _ diffs: [Int],
+    _ times: [Int],
+    _ limit: Int64
+) -> Int {
+    var low = 0
+    var high = Int(1e9)
+    
+    while low <= high {
+        let mid = (low + high) / 2
+        var prev = 0
+        var total = 0
+        
+        for i in 0..<diffs.count {
+            let gap = diffs[i] - mid
+            if gap > 0 {
+                total += (prev + times[i]) * gap + times[i]
+            } else {
+                total += times[i]
+            }
+            prev = times[i]
+        }
+        
+        if total > limit {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    
+    return low == 0 ? 1 : low
+}
