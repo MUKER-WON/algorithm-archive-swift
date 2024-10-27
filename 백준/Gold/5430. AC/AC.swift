0ce9c08dd5a20fb205c1820590a49ce806a50fa3
Kang Muk <muker.dev@gmail.com>
2024-10-27 14:46:31 +0900

    [Gold V] Title: AC, Time: 208 ms, Memory: 77908 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.swift
+++ 백준/Gold/5430. AC/AC.swift
@@ -1,20 +1,8 @@
-loop: for _ in 0..<Int(readLine()!)! {
+for _ in 0..<Int(readLine()!)! {
     let p = readLine()!
     _ = readLine()
     let a = readLine()!.split { $0 == "[" || $0 == "]" || $0 == "," }
     var (t,l,r) = (false,0,a.count)
-    for c in p {
-        if c == "R" {
-            t.toggle()
-        } else {
-            if l > r {
-                print("error")
-                continue loop
-            }
-            if t { r -= 1 } else { l += 1 }
-        }
-    }
-    if l > r { print("error") } else {
-        print("[\(t ? a[l..<r].reversed().joined(separator: ",") : a[l..<r].joined(separator: ","))]")
-    }
+    for c in p { if c == "R" { t.toggle() } else { if t { r -= 1 } else { l += 1 } } }
+    if l > r { print("error") } else { print("[\(t ? a[l..<r].reversed().joined(separator: ",") : a[l..<r].joined(separator: ","))]") }
 }
