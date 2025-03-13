ca78c79fc71b90e46683893aed7c5d6e7a7c2e0d
Kang Muk <muker.dev@gmail.com>
2025-03-13 22:02:24 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.swift
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.swift
@@ -1,19 +1,15 @@
 let N = Int(readLine()!)!
 let MOD = 1_000_000_000
-var dp = Array(repeating: Array(repeating: 0, count: 10), count: N+1)
+var dp = [0,1,1,1,1,1,1,1,1,1]
 
-for j in 1...9 {
-    dp[1][j] = 1
-}
-
-if N > 1 {
-    for i in 2...N {
-        for j in 0...9 {
-            if j > 0 { dp[i][j] += dp[i-1][j-1] % MOD }
-            if j < 9 { dp[i][j] += dp[i-1][j+1] % MOD }
-            dp[i][j] %= MOD
-        }
+for _ in 1..<N {
+    var new = dp
+    (new[0],new[9]) = (dp[1],dp[8])
+    
+    for i in 1...8 {
+        new[i] = (dp[i-1]+dp[i+1]) % MOD
     }
+    dp = new
 }
 
-print(dp[N].reduce(0) { ($0 + $1) % MOD })
+print(dp.reduce(0) { ($0 + $1) % MOD })
