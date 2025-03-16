baf861e4ddc83c62d8b82d7e65f92efb70a964d5
Kang Muk <muker.dev@gmail.com>
2025-03-16 20:04:53 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.swift
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.swift
@@ -8,33 +8,23 @@ for _ in 0..<N+M {
 }
 
 var queue = [(1,0)]
-var ans = 20
+var ans = Int.max
 var visited = Array(repeating: false, count: 101)
 visited[1] = true
 
 while !queue.isEmpty {
     let n = queue.removeFirst()
     
-    if n.1 >= ans { continue }
     if n.0 >= 100 {
-        ans = min(ans, n.1)
-        continue
+        print(n.1)
+        break
     }
     
     for i in 1...6 {
         let nn = n.0+i
-        
         guard nn <= 100, !visited[nn] else { continue }
         visited[nn] = true
-        
-        if list[nn] != 0 {
-            queue.append((list[nn], n.1+1))
-        } else if i == 100 {
-            queue.append((nn, n.1+1))
-        } else {
-            queue.append((nn, n.1+1))
-        }
+        let ni = list[nn] == 0 ? nn : list[nn]
+        queue.append((ni, n.1+1))
     }
 }
-
-print(ans)
