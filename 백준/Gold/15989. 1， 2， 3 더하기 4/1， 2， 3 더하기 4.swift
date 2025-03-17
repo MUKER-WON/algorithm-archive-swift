d4405877246e94d1b2586aa07ef3b119db415a49
Kang Muk <muker.dev@gmail.com>
2025-03-17 19:30:40 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.swift
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.swift
@@ -1,14 +1,12 @@
-var dp = Array(repeating: [0,1,0,0], count: 10001)
+var dp = Array(repeating: 0, count: 10001)
+dp[0] = 1
 
-for i in 2...10000 {
-    dp[i][2] = dp[i-2][2] + dp[i-2][1]
+for i in 1...3 {
+    for j in i..<10001 {
+        dp[j] += dp[j-i]
+    }
 }
-
-for i in 3...10000 {
-    dp[i][3] = dp[i-3][3] + dp[i-3][2] + dp[i-3][1]
-}
-
 
 for _ in 0..<Int(readLine()!)! {
-    print(dp[Int(readLine()!)!].reduce(0,+))
+    print(dp[Int(readLine()!)!])
 }
